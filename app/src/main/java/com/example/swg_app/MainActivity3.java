b49620458908cff9b7779fc1aa180ef9c1352f46
Shashvat6264 <shashvat6264@gmail.com>
2020-06-11 20:16:20 +0530

    Added all links

--- app/src/main/java/com/example/swg_app/MainActivity3.java
+++ app/src/main/java/com/example/swg_app/MainActivity3.java
@@ -13,13 +13,8 @@ public class MainActivity3 extends AppCompatActivity {
     private static final String TAG = "MainActivity3";
 
     GridView gridView;
+    private String[] url_id={"https://medium.com/@swgiitkgp/know-your-department-aerospace-engineering-2f56a3572ffe","https://medium.com/@swgiitkgp/know-your-department-agricultural-and-food-engineering-99f88ff9d8a8","https://medium.com/@swgiitkgp/know-your-department-biotechnology-2e105384ce43","https://medium.com/@swgiitkgp/know-your-department-chemistry-ce8973b56a67","https://medium.com/@swgiitkgp/know-your-department-chemical-engineering-c54d2bc608d5","https://medium.com/@swgiitkgp/know-your-department-civil-engineering-63caabd97cb0","https://medium.com/@swgiitkgp/know-your-department-computer-science-and-engineering-24392aab7146","https://medium.com/@swgiitkgp/know-your-department-electronics-and-electrical-communications-engineering-4bf6a043475","https://medium.com/@swgiitkgp/know-your-department-electrical-engineering-7a66cc2a80d3","https://medium.com/@swgiitkgp/know-your-department-geology-and-geophysics-31fd09664b3d","https://medium.com/@swgiitkgp/know-your-branch-exploration-geophysics-dd30c110b444","https://medium.com/@swgiitkgp/know-your-branch-economics-67e8b350a5de","https://medium.com/@swgiitkgp/know-your-department-industrial-and-systems-engineering-50fe3321bd6d","https://medium.com/@swgiitkgp/know-your-branch-instrumentation-engineering-726b62c0ce78","https://medium.com/@swgiitkgp/know-your-department-mechanical-engineering-bf4aaa54e8d","https://medium.com/@swgiitkgp/know-your-branch-manufacturing-science-and-engineering-894e2a2346d2","https://medium.com/@swgiitkgp/know-your-department-mining-engineering-2c37de00793f","https://medium.com/@swgiitkgp/know-your-department-mathematics-d5ad3ff992f4","https://medium.com/@swgiitkgp/know-your-department-metallurgical-and-materials-engineering-26d1070cec97","https://medium.com/@swgiitkgp/know-your-department-ocean-engineering-and-naval-architecture-82ebca5c6bdf","https://medium.com/@swgiitkgp/know-your-department-physics-da4bdb49af6b","https://medium.com/@swgiitkgp/know-your-branch-quality-engineering-design-and-manufacturing-69329b687d2e"};
 
-    private String[] url_id = {"http://www.facebook.com","http://www.google.com","http://www.google.com","http://www.google.com",
-            "http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com",
-            "http://www.google.com","http://www.google.com","http://www.facebook.com","http://www.google.com","http://www.google.com",
-            "http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com","http://www.google.com",
-            "http://www.google.com","http://www.google.com","http://www.facebook.com"
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
